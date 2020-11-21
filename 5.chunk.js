c380e0711f23ee91a4a1193881930141f86170e8
LBN <nhutlbuit@LBNs-MacBook-Pro.local>
2020-11-21 22:48:36 +0700

    Updates

---
+++ 5.chunk.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{77:function(c,n,i){"use strict";i.d(n,"a",(function(){return o})),i.d(n,"b",(function(){return e}));
+/*!
+ * Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ */
+var o={prefix:"fas",iconName:"caret-down",icon:[320,512,[],"f0d7","M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"]},e={prefix:"fas",iconName:"caret-up",icon:[320,512,[],"f0d8","M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"]}}}]);
